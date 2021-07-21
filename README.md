255f21b0ce8c1e64d7ba73ad154c46730aa7496a
Kauã Maximino <kaua_maximino@outlook.com>
2021-07-21 16:48:16 -0300

    Finalização do projeto com todas funcionalidades. Temperatura ok, direção e velocidade do vento ok.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # clima
+
+Projeto de um sistema para consulta do clima no mundo todo, usando a API do WeatherOne para consulta
+
+# Tecnologias utilizadas
+JS, HTML e CSS
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     let input = document.querySelector('#searchInput').value
 
     if (input !== "") {
+        clearInfo();
         showWarning('Carregando...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`;
@@ -22,6 +23,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 
             })
         } else {
+            clearInfo();
             showWarning('Localização não encontrada.')
         }
     }
@@ -31,7 +33,20 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 function showInfo(json) {
     showWarning('');
 
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
     document.querySelector('.resultado').style.display = 'block'
+}
+
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
 }
 
 function showWarning(msg) {
